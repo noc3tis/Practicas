ca13d9f76fff709890f21494167423838418472e
noc3tis <adrain150604@gmail.com>
2025-09-18 23:36:19 -0600

    Practica 3 Terminada.

--- app/src/main/java/com/example/practicas/MainActivity.kt
+++ app/src/main/java/com/example/practicas/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -24,6 +25,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.contentColorFor
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -49,80 +51,88 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 )  {
-                    Practica()
+                    Interfaz()
                 }
             }
         }
     }
 }
 
+data class valoresISR(
+    val limiteInferior: Double,
+    val limiteSuperior: Double,
+    val cuotaFija: Double,
+    val porcentajeExcedente: Double
+)
 
-@Preview(showBackground = true)
+val ISRs = listOf(
+    valoresISR(0.01, 746.04, 0.00, 1.92),
+    valoresISR(746.04, 6332.05, 14.32, 6.40),
+    valoresISR(6332.06, 11128.01, 371.83, 10.88),
+    valoresISR(11128.02, 12935.82, 893.63, 16.00),
+    valoresISR(12935.83, 15487.71, 1182.88, 17.92),
+    valoresISR(15487.72, 31236.49, 1640.18, 21.36),
+    valoresISR(31236.50, 49233.00, 5004.12, 23.52),
+    valoresISR(49233.01, 93993.90, 9236.89, 30.00),
+    valoresISR(93993.91, 125325.20, 22665.17, 32.00),
+    valoresISR(125325.21, 375975.61, 32691.18, 34.00),
+    valoresISR(375975.62, Double.MAX_VALUE, 117912.32, 35.00)
+)
+
+fun calcularISR(sueldo: Double, rangos: List<valoresISR>): Double {
+    val rango = rangos.firstOrNull { sueldo in it.limiteInferior..it.limiteSuperior }
+    return if (rango != null) {
+        (sueldo - rango.limiteInferior) * (rango.porcentajeExcedente / 100)  + rango.cuotaFija
+    } else 0.0
+}
+
+
 @Composable
-fun Practica() {
-    var texto = ""
-    val context = LocalContext.current
-    var valora by remember { mutableStateOf("") }
-    var valorb by remember { mutableStateOf("") }
-    var resultado by remember { mutableStateOf("") }
-    Column(modifier = Modifier.fillMaxWidth(),
+fun Interfaz() {
+    var texto by remember { mutableStateOf("") }
+    var ISR by remember { mutableStateOf("") }
+    var sueldo by remember { mutableStateOf("") }
+    Column (
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-        Row() {
-            OutlinedTextField(
-                value = texto,
-                onValueChange = { texto = it },
-                label = { Text("Nombre") },
-                modifier = Modifier
-                    .fillMaxWidth(0.8f), // 80% del ancho de la pantalla
-                textStyle = androidx.compose.ui.text.TextStyle(
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center
-                )
-            )
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        OutlinedTextField(
+            value = texto,
+            onValueChange = {texto = it},
+            label = {Text(text = "Sueldo Diario")}
+        )
 
-        }
-        Row() {
-            Button(onClick = {/*TODO*/
-                Toast.makeText(context, texto, Toast.LENGTH_LONG).show()}, elevation = ButtonDefaults.buttonElevation(defaultElevation = 20.dp, pressedElevation = 15.dp, disabledElevation = 0.dp), border = BorderStroke(3.dp, Color.Black)
-            ) {
-                Text("Enviar")
-            }
-        }
-        Row(Modifier.padding(16.dp)){
-            OutlinedTextField(
-               value = valora,
-                label = {Text("Primer Valor")},
-                onValueChange = {valora = it}
-               )
+        Spacer(modifier = Modifier.padding(10.dp))
 
+        OutlinedTextField(
+            value = ISR,
+            onValueChange = {ISR = it},
+            label = {Text(text = "ISR")},
+            readOnly = true
+        )
 
-        }
-        Row (Modifier.padding(16.dp)){
-            OutlinedTextField(
-                value = valorb,
-                label = {Text("Segundo valor")},
-                onValueChange = {valorb = it}
-            )
-        }
-        Row(Modifier.align(Alignment.CenterHorizontally)) {
-            OutlinedButton(onClick = {/*TODO*/
-            val a = valora.toInt()
-            val b = valorb.toInt()
-            val c = a + b
-            resultado = c.toString()
-            }) {
-                Text(text = "Enviar")
-            }
-        }
-        Row(Modifier.padding(16.dp)){
-            OutlinedTextField(
-                value = resultado,
-                label = {Text("Resultado")},
-                onValueChange = {resultado = it}
-            )
+        Spacer(modifier = Modifier.padding(10.dp))
+
+        OutlinedTextField(
+            value = sueldo,
+            onValueChange = {sueldo = it},
+            label = {Text(text = "Sueldo")},
+            readOnly = true
+        )
+
+        Spacer(modifier = Modifier.padding(5.dp))
+
+        Button(onClick = {
+            val sueMens = texto.toDoubleOrNull() ?: 0.0
+            val resultTemo = calcularISR(sueMens, ISRs)
+            val result = sueMens - resultTemo
+
+            ISR = String.format("%.2f", resultTemo)
+            sueldo = String.format("%.2f", result)
+
+        }, modifier = Modifier) {
+            Text(text = "Calcular")
         }
     }
 }
